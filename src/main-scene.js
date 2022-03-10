6f52b925bbe17ae78d1aaf873bf77f5fc028d176
Harrison Cassar <harrison.cassar@gmail.com>
2022-03-10 15:05:07 -0800

    Re-worked file tree/naming to respect new shadows support. -Remove old attempt at shadows support. -Rename shadow-supporting code to `mars_rover.js` as desired.

--- src/main-scene.js
+++ src/main-scene.js
@@ -8,8 +8,7 @@ import {Surfaces_Demo} from "./examples/surfaces-demo.js"
 import {Text_Demo} from "./examples/text-demo.js"
 import {Transforms_Sandbox} from "./examples/transforms-sandbox.js"
 import {Mars_Rover} from "./mars_rover.js"
-import {Mars_Rover_Shadow} from "./mars_rover_shadow.js"
-import {Mars_Rover_Shadows_Attempted} from "./mars_rover_shadows_attempted.js"
+import {Mars_Rover_No_Shadow} from "./mars_rover_no_shadow.js"
 import {Shadow_Demo} from "./examples/shadow-demo.js"
 import {Control_Demo} from "./examples/control-demo.js"
 import {Mouse_Demo} from "./examples/mouse-demo.js"
@@ -40,8 +39,7 @@ Object.assign(defs,
     {Text_Demo},
     {Transforms_Sandbox},
     {Mars_Rover},
-    {Mars_Rover_Shadow},
-    {Mars_Rover_Shadows_Attempted},
+    {Mars_Rover_No_Shadow},
     {Shadow_Demo},
     {Mouse_Demo},
     {Control_Demo}
@@ -51,8 +49,7 @@ Object.assign(defs,
 
 // (Can define Main_Scene's class here)
 
-//const Main_Scene = Mars_Rover_Shadows_Attempted;
-const Main_Scene = Mars_Rover_Shadow;
+const Main_Scene = Mars_Rover;
 const Additional_Scenes = [];
 
 export {Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs}
